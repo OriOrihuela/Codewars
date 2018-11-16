f27798627f9b4f3453a61cbf52e7e9ae82363c3f
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2018-11-16 20:40:05 +0100

    All test cases passed except the ones from Codewars

--- katas_6kyu/Are_they_the_same.py
+++ katas_6kyu/Are_they_the_same.py
@@ -7,8 +7,12 @@ def comp(array1, array2):
         return False
     
     for i in array1:
-        if i ** 2 in array2:
-            return True
+        if array2 and ( i ** 2 not in array2 ):
+            return False
+        else:
+            array2.remove(i**2)
+    
+    return True
 
 
 
@@ -25,11 +29,34 @@ if __name__ == "__main__":
     else:
         print ("Test case not passed")
 
-    # CASOS TEST
-    a3= []
-    a4 = []
+    a3 = [81, 99, 34, 11]
+    a4 = [81*81, 99*99, 34*34, 44*44]
 
-    if comp(a3, a4):
+    if not comp(a3, a4):
+        print ("Test case passed")
+    else:
+        print ("Test case not passed")
+
+    a5= []
+    a6 = []
+
+    if not comp(a5, a6):
+        print ("Test case passed")
+    else:
+        print ("Test case not passed")
+
+    a7 = [121, 144, 19, 11]
+    a8 = [11*11, 121*121, 144*144, 19*19]
+
+    if comp(a7, a8):
+        print ("Test case passed")
+    else:
+        print ("Test case not passed")
+
+    a9 = [12, 15, 77, 56, 87]
+    a10 = [12*12, 12*12, 77*77, 87*87, 56*56]
+
+    if not comp(a9, a10):
         print ("Test case passed")
     else:
         print ("Test case not passed")
