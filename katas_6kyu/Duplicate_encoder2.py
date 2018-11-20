65a7b18c00bb1008422c2b9b8f7f55690e5e08c8
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2018-11-20 18:02:55 +0100

    1st test case passed

---
+++ katas_6kyu/Duplicate_encoder2.py
@@ -0,0 +1,17 @@
+
+
+def duplicate_encode(word):
+    final_word = ""
+    for letter in word:
+        if word.count(letter) == 1:
+            final_word = final_word + "("
+    return final_word
+
+
+if __name__ == "__main__":
+    
+
+    # CASE TEST #
+
+
+    print(duplicate_encode("din"), ", should return (((")
