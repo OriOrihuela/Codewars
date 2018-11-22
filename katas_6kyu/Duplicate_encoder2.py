c1168d417aae504336529c9ab6beb9dbec001f79
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2018-11-22 12:14:27 +0100

    Kata resolved

--- katas_6kyu/Duplicate_encoder2.py
+++ katas_6kyu/Duplicate_encoder2.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     # TEST CASES #
 
 
-    assert(duplicate_encode("din")) == "((("
-    assert(duplicate_encode("recede")) == "()()()"
-    assert(duplicate_encode("Success")) == ")())())"
-    assert(duplicate_encode("(( @")) == "))(("
+    assert duplicate_encode("din") == "((("
+    assert duplicate_encode("recede") == "()()()"
+    assert duplicate_encode("Success") == ")())())"
+    assert duplicate_encode("(( @") == "))(("
