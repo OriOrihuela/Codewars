673ddcaffae56ca61b97d97f58e7614bfc40f41f
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2018-11-22 12:57:06 +0100

    2nd test case passed

--- katas_8kyu/I_love_you.py
+++ katas_8kyu/I_love_you.py
@@ -1,6 +1,5 @@
 '''Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the 
 following phrases each time a petal was torn:
-
 I love you
 a little
 a lot
