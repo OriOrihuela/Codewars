5a7dbe522afa7fefc845b3c8831427b7b9139020
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2018-11-28 19:12:56 +0100

    Kata resolved

--- katas_6kyu/Duplicate_encoder_dict.py
+++ katas_6kyu/Duplicate_encoder_dict.py
@@ -1,7 +1,20 @@
 
 def duplicate_encode(word):
-
-
+    
+    encode = {}
+    word = word.lower()
+    
+    for char in word:
+        if char not in encode:
+            encode[char] = "("
+        else:
+            encode[char] = ")"
+    
+    new_word = ""
+    
+    for char in word:
+        new_word += encode[char]
+    return new_word
 
 
 if __name__ == "__main__":
