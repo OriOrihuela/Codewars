26836ef187b6fd391f4246fa1fba00a943fb7401
Eduardo Orihuela Verdugo <44238128+OriOrihuela@users.noreply.github.com>
2018-12-20 14:51:01 +0100

    Update and rename Rock_paper_scissors.py to rock_paper_scissors.py

--- Python_katas/katas_8kyu/rock_paper_scissors.py
+++ Python_katas/katas_8kyu/rock_paper_scissors.py
@@ -31,12 +31,12 @@ if __name__ == "__main__":
     # TEST CASES #
 
 
-    print(rps('rock', 'scissors'))
-    print(rps('scissors', 'paper'))
-    print(rps('paper', 'rock'))
-    print(rps('scissors', 'rock'))
-    print(rps('paper', 'scissors'))
-    print(rps('rock', 'paper'))
-    print(rps('rock', 'rock'))
-    print(rps('scissors', 'scissors'))
-    print(rps('paper', 'paper'))
+    assert rps('rock', 'scissors') == "Player 1 won!"
+    assert rps('scissors', 'paper') == "Player 1 won!"
+    assert rps('paper', 'rock') == "Player 1 won!"
+    assert rps('scissors', 'rock') == "Player 2 won!"
+    assert rps('paper', 'scissors') == "Player 2 won!"
+    assert rps('rock', 'paper') == "Player 2 won!"
+    assert rps('rock', 'rock') == "Draw!"
+    assert rps('scissors', 'scissors') == "Draw!"
+    assert rps('paper', 'paper') == "Draw!"
