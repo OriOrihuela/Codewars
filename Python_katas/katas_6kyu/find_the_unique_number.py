fa83638d7b20912e00f80d6bc7e349ce296fb234
Eduardo Orihuela Verdugo <44238128+OriOrihuela@users.noreply.github.com>
2018-12-20 14:02:17 +0100

    Update and rename Find_the_unique_number.py to find_the_unique_number.py

--- Python_katas/katas_6kyu/find_the_unique_number.py
+++ Python_katas/katas_6kyu/find_the_unique_number.py
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     # TEST CASES #
 
 
-    assert (find_uniq([1, 1, 1, 2, 1, 1]) == 2)
-    assert (find_uniq([0, 0, 0.55, 0, 0]) == 0.55)
-    assert (find_uniq([3, 10, 3, 3, 3]) == 10)
+    assert find_uniq([1, 1, 1, 2, 1, 1]) == 2
+    assert find_uniq([0, 0, 0.55, 0, 0]) == 0.55
+    assert find_uniq([3, 10, 3, 3, 3]) == 10
