f42356aafc7073ed9172e17dbc78d4bb09d8116c
Eduardo Orihuela Verdugo <44238128+OriOrihuela@users.noreply.github.com>
2018-12-20 14:05:33 +0100

    Update and rename Loose_change.py to loose_change.py

--- Python_katas/katas_6kyu/loose_change.py
+++ Python_katas/katas_6kyu/loose_change.py
@@ -58,4 +58,4 @@ if __name__ == "__main__":
     assert loose_change(0) == {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 0}
     assert loose_change(91) == {'Nickels': 1, 'Pennies': 1, 'Dimes': 1, 'Quarters': 3}
     assert loose_change(29) == {'Nickels': 0, 'Pennies': 4, 'Dimes': 0, 'Quarters': 1}
-    assert(loose_change(3.9) == {'Nickels': 0, 'Pennies': 3, 'Dimes': 0, 'Quarters': 0})
+    assert loose_change(3.9) == {'Nickels': 0, 'Pennies': 3, 'Dimes': 0, 'Quarters': 0}
