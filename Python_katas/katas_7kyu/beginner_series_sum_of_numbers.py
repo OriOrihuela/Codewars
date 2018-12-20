9408bff53b068a42c54b4f4cd468eea4efe0243c
Eduardo Orihuela Verdugo <44238128+OriOrihuela@users.noreply.github.com>
2018-12-20 14:19:48 +0100

    Update and rename Beginner_series_sum_of_numbers.py to beginner_series_sum_of_numbers.py

---
+++ Python_katas/katas_7kyu/beginner_series_sum_of_numbers.py
@@ -0,0 +1,23 @@
+'''Given two integers a and b, which can be positive or negative, find the sum of all the numbers between 
+including them too and return it. 
+If the two numbers are equal return a or b.
+Note: a and b are not ordered!'''
+
+def get_sum(a,b):
+    if b > a:
+        return sum(range(a, b + 1))
+    else:
+        return sum(range(b, a + 1))
+
+
+if __name__ == "__main__":
+    
+
+    # TEST CASES #
+
+
+    assert get_sum(0,1) == 1
+    assert get_sum(0,-1) == -1
+    assert get_sum(5,54) == 1475
+    assert get_sum(0,78) == 3081
+    assert get_sum(-32,-1) == -528
