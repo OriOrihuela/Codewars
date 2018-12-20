c5792550e4c97b6344df5120dc57649b46fa2e58
Eduardo Orihuela Verdugo <44238128+OriOrihuela@users.noreply.github.com>
2018-12-20 14:15:37 +0100

    Yet trying to resolve

--- Python_katas/katas_5kyu/extract_the_domain_name_from_an_URL.py
+++ Python_katas/katas_5kyu/extract_the_domain_name_from_an_URL.py
@@ -1,4 +1,5 @@
-'''Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+'''Write a function that when given a URL as a string, 
+parses out just the domain name and returns it as a string. For example:
 
 domain_name("http://github.com/carbonfive/raygun") == "github" 
 domain_name("http://www.zombie-bites.com") == "zombie-bites"
