f5ff26b330f1b08fcb7140b64fe05b25b5bad4ce
Eduardo Orihuela Verdugo <44238128+OriOrihuela@users.noreply.github.com>
2018-12-20 14:11:13 +0100

    Yet trying to resolve

--- Python_katas/katas_6kyu/sort_the_odd.py
+++ Python_katas/katas_6kyu/sort_the_odd.py
@@ -23,5 +23,3 @@ if __name__ == "__main__":
     assert sort_array([]) == []
     assert sort_array([5, 3, 1, 8, 0]) == [1, 3, 5, 8, 0]
     assert sort_array([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
-    
-    
