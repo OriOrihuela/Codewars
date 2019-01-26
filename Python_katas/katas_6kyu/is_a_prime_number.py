ecd380277e92c217a5c709df38482633505807a4
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-26 19:35:19 +0100

    Description refactored

--- Python_katas/katas_6kyu/is_a_prime_number.py
+++ Python_katas/katas_6kyu/is_a_prime_number.py
@@ -1,6 +1,7 @@
 '''Define a function isPrime/is_prime() that takes one integer argument and returns true/True or false/False 
 depending on if the integer is a prime.
-Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.'''
+Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors 
+other than 1 and itself.'''
 
 def is_prime(number):
     if number in (0, 1):
