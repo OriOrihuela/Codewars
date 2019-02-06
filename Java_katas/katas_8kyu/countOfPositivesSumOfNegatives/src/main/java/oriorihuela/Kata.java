1da2e6806666e05ed65c911074f120f034c79e31
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-02-06 14:13:30 +0100

    Yet to pass 1 test case in Codewars

---
+++ Java_katas/katas_8kyu/countOfPositivesSumOfNegatives/src/main/java/oriorihuela/Kata.java
@@ -0,0 +1,36 @@
+/**
+ * Given an array of integers.
+ * Return an array, where the first element is the count of positives numbers and
+ * the second element is sum of negative numbers.
+ *
+ * If the input array is empty or null, return an empty array.
+ *
+ * Example
+ * For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+ */
+
+
+package oriorihuela;
+
+
+public class Kata {
+    public static int[] countPositivesSumNegatives(int[] input) {
+        if (input.length == 0 || input == null) {
+            return input;
+        }
+        else {
+            int[] arrayToReturn = new int[2];
+            int countOfPositiveOnes = 0;
+            int sumatoryOfNegativeOnes = 0;
+            for (int index = 0; index < input.length; index++) {
+                if (input[index] > 0) {
+                    countOfPositiveOnes++; }
+                else if (input[index] < 0) {
+                    sumatoryOfNegativeOnes += input[index]; }
+            }
+            arrayToReturn[0] = countOfPositiveOnes;
+            arrayToReturn[1] = sumatoryOfNegativeOnes;
+            return arrayToReturn;
+        }
+    }
+}
